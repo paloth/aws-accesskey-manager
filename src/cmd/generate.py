b661eef480cb48f745e8fe8b46a63c5fe8b989e3
paloth <lth.paul@hotmail.fr>
2020-06-25 11:55:25 +0200

    refactor: code to check token validity

--- src/cmd/generate.py
+++ src/cmd/generate.py
@@ -1,24 +1,28 @@
 import boto3
 import sys
 from ..internal import aws_config
-from ..internal.token import token_valid, get_sesion_token
+from ..internal import token
 
 
-def execute(profile_path, profile, user_name, token):
+def execute(profile_path, profile, user_name, user_token):
     profile_config = aws_config.get(profile_path)
 
     if not profile_config.has_section(profile):
-        sys.exit("The profile provided does not exists in your credential file\nPlease select a valid profile")
+        sys.exit(
+            "The profile provided does not exists in your credential file\nPlease select a valid profile"
+        )
+
+    while not token.validity(user_token):
+        print("The token must be composed by 6 digits")
+        user_token = input("Token:\n")
 
     session = boto3.session.Session(profile_name=profile)
 
-    if not token_valid(token):
-        print("The token must be composed by 6 digits")
-        token = input("Token:\n")
-
     sts = session.client("sts")
 
-    credentials = get_sesion_token(sts, user_name, token)
+    credentials = token.get_sesion_token(sts, user_name, user_token)
 
     aws_config.write(profile_path, profile, profile_config, credentials)
-    print(f"Profile [{profile}-tmp] has been updated and will expire on {credentials['Credentials']['Expiration']}")
+    print(
+        f"Profile [{profile}-tmp] has been updated and will expire on {credentials['Credentials']['Expiration']}"
+    )
