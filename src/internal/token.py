f60203ec61902f9a15bfb9f5f63f531dbcef4fcc
paloth <lth.paul@hotmail.fr>
2020-06-25 11:58:28 +0200

    fix: Regex to check token

--- src/internal/token.py
+++ src/internal/token.py
@@ -2,8 +2,8 @@ import re
 from botocore.exceptions import ClientError
 
 
-def token_valid(token):
-    return True if re.match(r"\d{6}", token) else False
+def validity(token):
+    return True if re.match(r"^\d{6}$", token) else False
 
 
 def get_sesion_token(sts, user_name, user_token):
@@ -14,7 +14,8 @@ def get_sesion_token(sts, user_name, user_token):
 
     try:
         response = sts.get_session_token(
-            SerialNumber=f"arn:aws:iam::{identity_response['Account']}:mfa/{user_name}", TokenCode=user_token,
+            SerialNumber=f"arn:aws:iam::{identity_response['Account']}:mfa/{user_name}",
+            TokenCode=user_token,
         )
     except ClientError as error:
         raise error
