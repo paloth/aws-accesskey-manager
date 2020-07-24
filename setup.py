d0a66afaf88cd9d66ed8c8a204fe8bf1fb2b41a9
paloth <lth.paul@hotmail.fr>
2020-07-24 11:06:12 +0200

    build: update setup informations

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
 setup(
+    author="Paloth (https://github.com/paloth)",
+    url="https://github.com/paloth/aws-accesskey-manager",
     name="aws-key-management",
-    version="0.1",
-    py_module=["mfa"],
+    version="1.0",
+    description="Manage your personnal access key on AWS",
+    packages=["src", "src.cmd", "src.internal"],
+    # py_module=["mfa"],
     install_requires=["Click", "boto3", "botocore"],
+    license="MIT License",
     entry_points="""
     [console_scripts]
     akm=main:run
