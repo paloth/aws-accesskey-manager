651d45a655a36489b2f926b890ab66f15d5ac653
paloth <lth.paul@hotmail.fr>
2020-07-26 13:51:04 +0200

    add rotate cmd and rename import

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 
 import click
-from .cmd.mfa import execute
+
+
+from .cmd import cmdRotate, cmdMfa
 import sys
 
 AWS_PROFILE_FILE = f"{str(Path.home())}/.aws/credentials"
@@ -25,7 +27,7 @@ def run():
 @click.option("-u", "--user", required=True, help="AWS user name")
 @click.option("-t", "--token", required=True, help="MFA Token")
 def mfa(profile, user, token):
-    execute(AWS_PROFILE_FILE, profile, user, token)
+    cmdMfa.execute(AWS_PROFILE_FILE, profile, user, token)
 
 
 @run.command(help="Create a configuration to manage your personnal access key")
@@ -37,6 +39,15 @@ def config():
 
 
 @run.command(help="Rotate your personnal access key")
-# @click.option("-e", "--expire", help="Number of day before the access key must be change")
-def rotate():
-    sys.exit("This feature will be implemented soon")
+@click.option("-e", "--expire", help="Number of day before the access key must be change")
+@click.option(
+    "-p", "--profile", default=os.environ.get("AWS_PROFILE", "default"), show_default=True, help="AWS profile to use",
+)
+@click.option(
+    "-d", "--deactivate", default=False, show_default=True, help="Deactivate the old Access Key instead of delete it",
+)
+@click.option(
+    "-y", "--yes", default=False, show_default=True, help="Validate action without prompt",
+)
+def rotate(expire, profile, deactivate, yes):
+    cmdRotate.execute(expire, profile, deactivate, yes)
