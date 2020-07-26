4a28271311bd3e1a8452dc597ae69650dc823ca0
paloth <lth.paul@hotmail.fr>
2020-07-26 13:49:33 +0200

    refactor: name file cause some conflict

--- src/cmd/cmdMfa.py
+++ src/cmd/cmdMfa.py
@@ -8,9 +8,7 @@ def execute(profile_path, profile, user_name, user_token):
     profile_config = aws_config.get(profile_path)
 
     if not profile_config.has_section(profile):
-        sys.exit(
-            f"The profile {profile} does not exists in your credential file\nPlease select a valid profile"
-        )
+        sys.exit(f"The profile {profile} does not exists in your credential file\nPlease select a valid profile")
 
     while not token.validity(user_token):
         print("The token must be composed by 6 digits")
@@ -23,6 +21,4 @@ def execute(profile_path, profile, user_name, user_token):
     credentials = token.get_sesion_token(sts, user_name, user_token)
 
     aws_config.write(profile_path, profile, profile_config, credentials)
-    print(
-        f"Profile [{profile}-tmp] has been updated and will expire on {credentials['Credentials']['Expiration']}"
-    )
+    print(f"Profile [{profile}-tmp] has been updated and will expire on {credentials['Credentials']['Expiration']}")
