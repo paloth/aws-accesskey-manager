3a753e48f707d06fbad384a3d180c6184eeefb51
paloth <lth.paul@hotmail.fr>
2020-07-28 22:50:27 +0200

    refactor: remove useless part of the code

--- src/internal/keymgt.py
+++ src/internal/keymgt.py
@@ -1,20 +1,10 @@
 from datetime import datetime, timedelta
 
-import boto3
-from botocore.exceptions import ClientError
 from dateutil.tz import tzutc
 from . import aws_config
 import sys
 
 """Access Key management"""
-
-
-def convert_to_date(date_to_convert):
-    """Convert the date format """
-    try:
-        return datetime.strptime(date_to_convert, "%Y-%m-%dT%H:%M:%S")
-    except ValueError as error:
-        raise error
 
 
 def is_access_key_expired(date, days):
@@ -54,32 +44,3 @@ def renew(config, iam, deactivate, profile, user_name):
         iam.delete_access_key(UserName=user_name, AccessKeyId=current_ak)
 
     return new_ak
-
-
-def check(config, user_name, profile, path):
-    session = boto3.session.Session(profile_name=f"{profile}-tmp")
-    iam = session.client("iam")
-    try:
-        current_keys = iam.list_access_keys(UserName=user_name)
-    except ClientError as error:
-        raise error
-
-    for key in current_keys["AccessKeyMetadata"]:
-        if is_access_key_expired(key["CreateDate"]):
-            new_key = iam.create_access_key(UserName=user_name)
-            if aws_config.update_profile(path, profile, config, new_key):
-                try:
-                    iam.update_access_key(
-                        UserName=user_name, AccessKeyId=key["AccessKeyId"], Status="Inactive",
-                    )
-                except ClientError as error:
-                    raise error
-
-                print("Your access key is expired and has been updated")
-            else:
-                return False
-        else:
-            remaining_days = (key["CreateDate"] + timedelta(days=90)) - datetime.now().replace(tzinfo=tzutc())
-            print(f"Your access key will expire in {remaining_days.days} days ")
-
-    return True
