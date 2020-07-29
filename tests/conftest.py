8ae6f15eb30c5528c2635565783b3d944bed343a
paloth <lth.paul@hotmail.fr>
2020-07-29 16:19:53 +0200

    test: improve test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,17 @@ from pytest import fixture
 
 @fixture()
 def sts_get_session_response():
-    return {
-        "Credentials": {
-            "AccessKeyId": "accesskey",
-            "SecretAccessKey": "secretkey",
-            "SessionToken": "sessiontoken",
-        }
-    }
+    return {"Credentials": {"AccessKeyId": "accesskey", "SecretAccessKey": "secretkey", "SessionToken": "sessiontoken"}}
 
 
 @fixture()
 def sts_get_caller_id_response():
     return {"UserId": "string", "Account": "000000000000", "Arn": "string"}
+
+
+@fixture()
+def boto_standard_error():
+    return {"Error": {"Code": "WhatEver", "Message": "Error"}}
 
 
 @fixture()
--- tests/test_token.py
+++ tests/test_token.py
@@ -6,8 +6,10 @@ from unittest.mock import MagicMock
 
 
 def test_token_validity():
-    assert validity("000000") is True
+    assert validity("123456") is True
     assert validity("12345") is False
+    assert validity("1234567") is False
+    assert validity("qwerty") is False
 
 
 def test_get_session_token(sts_get_session_response, sts_get_caller_id_response):
@@ -18,13 +20,18 @@ def test_get_session_token(sts_get_session_response, sts_get_caller_id_response)
     assert get_sesion_token(sts, "user", "000000") == sts_get_session_response
 
 
-def test_get_session_token_fail(
-    sts_get_session_response, sts_get_caller_id_response, sts_get_session_error
-):
+def test_get_caller_id_fails(sts_get_session_response, sts_get_caller_id_response, boto_standard_error):
     sts = MagicMock()
-    sts.get_session_token = MagicMock(
-        side_effect=ClientError(sts_get_session_error, "Error")
-    )
+    sts.get_caller_identity = MagicMock(side_effect=ClientError(boto_standard_error, "Error"))
+
+    with pytest.raises(ClientError) as error:
+        get_sesion_token(sts, "user", "000000")
+    assert "WhatEver" in str(error.value)
+
+
+def test_get_session_token_fails(sts_get_session_response, sts_get_caller_id_response, boto_standard_error):
+    sts = MagicMock()
+    sts.get_session_token = MagicMock(side_effect=ClientError(boto_standard_error, "Error"))
     sts.get_caller_identity.return_value = sts_get_caller_id_response
 
     with pytest.raises(ClientError) as error:
